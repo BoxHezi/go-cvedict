a09aaa09ca018a4f08363974501d7514ceb5a7ab
boxhezi <547181293@qq.com>
2024-06-14 10:39:22 +1000

    add TestDatabaseConnection in PersistentPreRun

--- main.go
+++ main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"log"
-
 	cmd "cve-dict/services/cmd"
 )
 
 func main() {
-	rootCmd, err := cmd.InitCmd()
-	if err != nil {
-		log.Fatal(err)
-	}
+	rootCmd := cmd.InitCmd()
 	rootCmd.Execute()
 }
--- services/cmd/cmd.go
+++ services/cmd/cmd.go
@@ -2,7 +2,10 @@ package cmd
 
 import (
 	"fmt"
-	"os"
+	"log"
+
+	// "log"
+	// "os"
 	"slices"
 
 	"github.com/spf13/cobra"
@@ -13,43 +16,41 @@ import (
 	model "cve-dict/model"
 )
 
-func InitCmd() (*cobra.Command, error) {
+func InitCmd() *cobra.Command {
 	rootCmd, rootFlags := initRootCmd()
-
-	// test datasbase connection
-	uri := db.ConstructUri(*rootFlags.GetAddressP(), *rootFlags.GetPortP())
-	client := db.Connect(uri)
-	defer db.Disconnect(client)
-	if err := db.TestConnection(client); err != nil {
-		return nil, err
-	}
 
 	updateCmd := initUpdateCmd(rootFlags)
 	fetchCmd := initFetchCmd(rootFlags)
 
 	rootCmd.AddCommand(updateCmd, fetchCmd)
-	return rootCmd, nil
+	return rootCmd
 }
 
 func initRootCmd() (*cobra.Command, *model.CmdFlags) {
+	var flags *model.CmdFlags = new(model.CmdFlags)
 	rootCmd := &cobra.Command{
 		Use:   "cve-dict",
 		Short: "CVE dict",
 		Long:  "Local CVE Dictionary",
-		Run:   func(cmd *cobra.Command, args []string) {},
+		// Run:   func(cmd *cobra.Command, args []string) {},
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("Testing Database Connection... - mongodb://%s:%d\n", *flags.GetAddressP(), *flags.GetPortP())
+			uri := db.ConstructUri(*flags.GetAddressP(), *flags.GetPortP())
+			client := db.Connect(uri)
+			defer db.Disconnect(client)
+
+			if err := db.TestConnection(client); err != nil {
+				log.Fatalf("Database connection failed: %s", err)
+			}
+		},
 	}
-
-	var flags *model.CmdFlags = new(model.CmdFlags)
-
-	rootCmd.PersistentFlags().StringVarP(flags.GetAddressP(), "address", "a", "127.0.0.1", "database host")
+	rootCmd.PersistentFlags().StringVarP(flags.GetAddressP(), "address", "a", "127.0.0.1", "database address")
 	rootCmd.PersistentFlags().Uint32VarP(flags.GetPortP(), "port", "p", 27001, "database port")
 	rootCmd.PersistentFlags().StringVarP(flags.GetDatabaseP(), "database", "d", "", "database name")
 	rootCmd.PersistentFlags().StringVarP(flags.GetCollectionP(), "collection", "c", "", "collection name")
 
-	rootCmd.MarkFlagRequired("database")
-	rootCmd.MarkFlagRequired("collection")
-
-	rootCmd.ParseFlags(os.Args[1:]) // manually parse flags
+	rootCmd.MarkPersistentFlagRequired("database")
+	rootCmd.MarkPersistentFlagRequired("collection")
 
 	return rootCmd, flags
 }
